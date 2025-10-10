a9a984615719a829a745e6216ce5280470124984
hoantxhe180030 <hoantxhe180030@fpt.edu.vn>
2025-10-10 09:23:23 +0700

    feat: init bazel and sample service

---
+++ services/sample/src/main/java/com/sample/Sample.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello Bazel!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
