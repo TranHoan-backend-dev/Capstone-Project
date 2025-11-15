7b99ef0910af709ea4b3fb482ae9828a72af9701
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-11-15 22:36:23 +0700

    fix: fix CI error #2

--- be/services/sample-python/test.py
+++ be/services/sample-python/test.py
@@ -1,2 +1,11 @@
+import unittest
+from main import some_function  # import hàm bạn muốn test từ main.py
+
+class SampleTest(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+        # ví dụ test thêm hàm trong main.py
+        # self.assertEqual(some_function(), expected_value)
+
 if __name__ == "__main__":
-    print("Hello World")
+    unittest.main()
