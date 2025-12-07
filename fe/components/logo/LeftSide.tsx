a30954315582f43873a97af06b5f1ae8ffcb2f9e
NguyenDucDungHE171774 <dungndhe171774@fpt.edu.vn>
2025-12-08 01:35:04 +0700

    feat: add interface for login page

---
+++ fe/components/logo/LeftSide.tsx
@@ -0,0 +1,9 @@
+export default function LeftSide() {
+  return (
+    <div className="w-1/2 flex flex-col justify-center items-center pr-6 border-r">
+      <img src="/next.svg" className="w-20 h-20 mb-3" />
+      <h1 className="text-3xl font-bold text-blue-600">CRM</h1>
+      <p className="text-sm text-gray-500 mt-1">Phiên bản 1.0.0</p>
+    </div>
+  );
+}
