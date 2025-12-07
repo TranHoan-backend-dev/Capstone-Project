61b3a53bb5f891a8d4ac6ef7b2f1eaa4d890ba9d
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-12-07 17:18:34 +0700

    feat: create sending email module for creating new account

--- be/apps/email-service/src/main.ts
+++ be/apps/email-service/src/main.ts
@@ -3,14 +3,34 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   // Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
