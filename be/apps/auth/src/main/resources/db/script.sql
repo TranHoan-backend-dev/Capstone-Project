ee2622d90628fe57e407927a8c5f5b0b2caf871c
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-01-24 10:37:18 +0700

    refactor: update DB script

--- be/apps/auth/src/main/resources/db/script.sql
+++ be/apps/auth/src/main/resources/db/script.sql
@@ -6,7 +6,7 @@ $$
     role_id3 uuid := 'da4429b4-d7a7-4283-b386-189d69a95af9';
     role_id4 uuid := 'a789aaf0-6153-4f30-ac38-b08e859d5fcb';
     role_id5 uuid := '724184e3-456a-4c89-b3c5-b8ab508f80b5';
-
+--
     user_id1 uuid := 'e78050e5-9e87-46d1-a250-957bfeece3f9';
     user_id2 uuid := '14c5879c-a6c4-45a6-846b-39d2b9d8c961';
   BEGIN
@@ -19,9 +19,10 @@ $$
 
     insert into users
     values (user_id1, ARRAY ['asdasd', 'asdasd', 'asdasd'], now(), 'eh', 'a@gmail.com',
-            'dfgsdfg', ARRAY ['asdasd', 'asdasd', 'asdasd'], 'iugkgvkhv', now(), 'asda', 'sg', role_id1),
+            true, true, 'dfgsdfg', null, null, ARRAY ['asdasd', 'asdasd', 'asdasd'], 'iugkgvkhv', now(), 'asda', 'sg',
+            role_id1),
            (user_id2, ARRAY ['asdasd', 'asdasd', 'asdasd'], now(), 'eh', 'b@gmail.com',
-            'dfgsdfg', ARRAY ['asdasd', 'asdasd', 'asdasd'], 'iugkgvkhv', now(), 'asf', 'sg',
+            false, false, 'dfgsdfg', null, null, ARRAY ['asdasd', 'asdasd', 'asdasd'], 'iugkgvkhv', now(), 'asf', 'sg',
             role_id2);
 
     insert into profile
