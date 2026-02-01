a8468e45bb567897e34736a0a6d9b2037dd41e0c
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-02-01 10:23:32 +0700

    fix: allow public accessing for swagger in the device service

--- be/apps/device/src/main/java/com/capstone/device/infrastructure/config/SecurityConfig.java
+++ be/apps/device/src/main/java/com/capstone/device/infrastructure/config/SecurityConfig.java
@@ -1,9 +1,6 @@
-package com.capstone.auth.infrastructure.config;
+package com.capstone.device.infrastructure.config;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,22 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -43,12 +30,7 @@ public class SecurityConfig {
   }
 
   CorsProperties corsProperties;
-  JwtDecoder decoder;
   final String[] PUBLIC_URLS = {
-      "/auth/**",
-      "/login/oauth2/code/google",
-      "/oauth2/authorization/google",
-      "/actuator/**",
       "/v3/api-docs/**",
       "/swagger-ui/**",
       "/public/**",
@@ -74,27 +56,7 @@ public class SecurityConfig {
               response.setStatus(HttpStatus.FORBIDDEN.value());
               response.getWriter().write("{\"error\": \"Access denied\"}");
             }))
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(decoder)))
         .build();
-  }
-
-  @Bean
-  public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
-    var delegate = new DefaultOAuth2UserService();
-    return userRequest -> {
-      var user = delegate.loadUser(userRequest);
-      Set<GrantedAuthority> authorities = new HashSet<>();
-
-      String email = user.getAttribute("email");
-      if (email != null) {
-        authorities.add(new SimpleGrantedAuthority(!email.endsWith("@fpt.edu.vn") ? "ROLE_USER" : "ROLE_ADMIN"));
-      }
-
-      return new DefaultOAuth2User(
-          authorities,
-          user.getAttributes(),
-          "email");
-    };
   }
 
   UrlBasedCorsConfigurationSource corsConfigurationSource() {
