b32f8bd4d7c235e2d8a872205c291944a08414b7
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-02-16 16:25:03 +0700

    feat: create the viewing calculation units use case

--- be/apps/device/src/main/java/com/capstone/device/application/business/boundary/UnitService.java
+++ be/apps/device/src/main/java/com/capstone/device/application/business/boundary/UnitService.java
@@ -1,4 +1,9 @@
 package com.capstone.device.application.business.boundary;
 
+import com.capstone.device.application.dto.response.UnitResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UnitService {
+  Page<UnitResponse> getPaginatedUnits(Pageable pageable, String filterName);
 }
