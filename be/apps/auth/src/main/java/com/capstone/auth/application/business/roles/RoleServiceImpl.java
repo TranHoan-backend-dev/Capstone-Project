bb92a468e229ff49389c898413e3a78d46a389e0
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-03-02 20:48:28 +0700

    refactor: move RoleName to the common library

--- be/apps/auth/src/main/java/com/capstone/auth/application/business/roles/RoleServiceImpl.java
+++ be/apps/auth/src/main/java/com/capstone/auth/application/business/roles/RoleServiceImpl.java
@@ -1,11 +1,11 @@
 package com.capstone.auth.application.business.roles;
 
 import com.capstone.auth.application.exception.NotExistingException;
-import com.capstone.auth.domain.enumerate.RoleName;
 import com.capstone.auth.domain.model.Roles;
 import com.capstone.auth.infrastructure.persistence.RoleRepository;
 import com.capstone.auth.infrastructure.config.Constant;
 import com.capstone.common.annotation.AppLog;
+import com.capstone.common.enumerate.RoleName;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
--- be/apps/auth/src/test/java/com/capstone/auth/application/business/users/UserServiceImplTest.java
+++ be/apps/auth/src/test/java/com/capstone/auth/application/business/users/UserServiceImplTest.java
@@ -2,13 +2,13 @@ package com.capstone.auth.application.business.users;
 
 import com.capstone.auth.application.dto.request.FilterUsersRequest;
 import com.capstone.auth.application.dto.response.EmployeeResponse;
+import com.capstone.common.enumerate.RoleName;
 import com.capstone.common.exception.ExistingException;
 import com.capstone.auth.application.exception.NotExistingException;
 import com.capstone.auth.domain.model.EmployeeJob;
 import com.capstone.auth.domain.model.Profile;
 import com.capstone.auth.domain.model.Roles;
 import com.capstone.auth.domain.model.Users;
-import com.capstone.auth.domain.enumerate.RoleName;
 import com.capstone.auth.infrastructure.persistence.BusinessPagesOfEmployeeRepository;
 import com.capstone.auth.infrastructure.persistence.EmployeeJobRepository;
 import com.capstone.auth.infrastructure.persistence.ProfileRepository;
