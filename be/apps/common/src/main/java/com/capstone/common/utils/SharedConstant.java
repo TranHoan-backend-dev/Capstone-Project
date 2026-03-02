15b6c2cd4f1376b4da39c15990d85314aa6dfe9b
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-03-02 20:48:28 +0700

    refactor: move RoleName to the common library

---
+++ be/apps/common/src/main/java/com/capstone/common/utils/SharedConstant.java
@@ -0,0 +1,7 @@
+package com.capstone.common.utils;
+
+public class SharedConstant {
+  public static final String EMAIL_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+  public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+  public static final String PHONE_PATTERN = "^[0-9]{10}$";
+}
