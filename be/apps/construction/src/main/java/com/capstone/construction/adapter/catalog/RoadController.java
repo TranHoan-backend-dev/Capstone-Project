0ae2f22d0b8e18180c265ef8b042dab0cca51871
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-03-09 22:48:01 +0700

    refactor: use environment variables instead of application-dev

--- be/apps/construction/src/main/java/com/capstone/construction/adapter/catalog/RoadController.java
+++ be/apps/construction/src/main/java/com/capstone/construction/adapter/catalog/RoadController.java
@@ -18,7 +18,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.slf4j.Logger;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
