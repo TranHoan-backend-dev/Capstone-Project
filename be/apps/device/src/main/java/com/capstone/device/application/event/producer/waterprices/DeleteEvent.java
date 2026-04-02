9c1dfad0beace576b2055509ee585d35bf29c6f9
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-04-02 23:06:08 +0700

    fix: add title for event consumer class

--- be/apps/device/src/main/java/com/capstone/device/application/event/producer/waterprices/DeleteEvent.java
+++ be/apps/device/src/main/java/com/capstone/device/application/event/producer/waterprices/DeleteEvent.java
@@ -1,14 +1,14 @@
-package com.capstone.device.application.event.producer.waterprices;
+// package com.capstone.device.application.event.producer.waterprices;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+// import java.math.BigDecimal;
+// import java.time.LocalDate;
 
-public record DeleteEvent(
-  String userTarget,
-  BigDecimal tax,
-  BigDecimal environmentPrice,
-  LocalDate applicationPeriod,
-  LocalDate expirationDate,
-  String description
-) {
-}
+// public record DeleteEvent(
+// String userTarget,
+// BigDecimal tax,
+// BigDecimal environmentPrice,
+// LocalDate applicationPeriod,
+// LocalDate expirationDate,
+// String description
+// ) {
+// }
